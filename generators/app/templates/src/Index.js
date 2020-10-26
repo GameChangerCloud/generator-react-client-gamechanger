1d8670aac4b0fe06a7645014e62402261cb7d619
Jérémy RATSIMANDRESY <jeremy.ratsimandresy@gmail.com>
2020-10-26 16:09:01 +0100

    ServiceWorker.js is no longer included in generated react app

--- generators/app/templates/src/Index.js
+++ generators/app/templates/src/Index.js
@@ -6,14 +6,9 @@ import { PersistGate } from 'redux-persist/integration/react'
 import Link from '@material-ui/core/Link';
 import { ToastContainer } from 'react-toastify'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { store, persistor } from './store/configureStore'
 import './index.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
 ReactDOM.render(
 	<Provider store={store}>
 		<ToastContainer />
