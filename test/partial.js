7c4dde7e157ddb5a4b89306991559dffe6b6e69d
Veyrack-IPPON <vlin@ippon.fr>
2021-05-12 15:03:49 +0200

    Adding mocha test

---
+++ test/partial.js
@@ -0,0 +1,8 @@
+var ejs = require('ejs')
+var chai = require('chai')
+
+chai.use(require('chai-string'))
+chai.use(require('chai-as-promised'))
+var should = chai.should()
+
+describe('partials', function () {})
